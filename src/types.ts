1439f7f88db81c117ab58acc98aafdd89d8885ac
Julien Oucli <xdrz.jot.sr@gmail.com>
2022-05-10 09:12:59 +0200

    feat: support Plotly types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,14 @@
 import { ChartJs } from '../deps.ts';
+import * as Plotly from '../vendor/Ploty/index.d.ts';
 
 export type PlotEngine = 'ChartJs' | 'Plotly' | 'GCharts';
 
+type ChartJsDatas = ChartJs.ChartConfiguration
+type PlotlyDatas = {data: Plotly.Data[], layout?: Partial<Plotly.Layout>, config?: Partial<Plotly.Config>}
+
+export type Datas = ChartJsDatas | PlotlyDatas
+
 export type Config = { title?: string; size: [number, number] };
-export type Datas = ChartJs.ChartConfiguration;
 export type Plot = { _id: string; datas: Datas; config: Config };
 export type DeplotOptions = {
   closeCallback: () => unknown;
