7d5d0612cdb688529b2251ff5b673933eae4903b
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-07-19 02:29:05 +0200

    refactor: :arrow_up: update WebUI version

--- deps.ts
+++ deps.ts
@@ -1,14 +1,8 @@
 export { serve } from 'https://deno.land/std@0.166.0/http/server.ts'
 export { ensureFile } from 'https://deno.land/std@0.166.0/fs/ensure_file.ts'
 export * as path from 'https://deno.land/std@0.166.0/path/mod.ts'
-// export { Webview, SizeHint } from 'https://deno.land/x/webview@0.7.5/mod.ts';
-//TODO Reuse official webview when windows multiple instances is fixed
-export {
-	SizeHint,
-	Webview,
-} from 'https://raw.githubusercontent.com/JOTSR/webview_deno/main/mod.ts'
-export { WebSocketServer } from 'https://deno.land/x/websocket@v0.1.4/mod.ts'
-export type { WebSocketClient } from 'https://deno.land/x/websocket@v0.1.4/mod.ts'
+// TODO move to next WebUI release
+export { WebUI } from 'https://raw.githubusercontent.com/webui-dev/deno-webui/2fa3b90752cb67c1bfa8377038db10f0ad413e19/mod.ts'
 export * as ChartJs from 'https://cdn.skypack.dev/chart.js@3.7?dts'
 import ensureVersion from 'https://deno.land/x/ensure_version@1.3.0/mod.ts'
 export * as esbuild from 'https://deno.land/x/esbuild@v0.15.16/mod.js'
