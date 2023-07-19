8bc82ef4de87f2a7a48ef198f06d8dec39e9d476
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-07-19 02:32:40 +0200

    refactor: :recycle: remove unused types

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,5 @@
 import { ChartJs } from '../deps.ts'
 import * as Plotly from '../vendor/Plotly/index.d.ts'
-import { Webview } from '../deps.ts'
 
 export type { ChartJs, Plotly }
 
@@ -19,7 +18,6 @@ export type Config = { title?: string; size: [number, number] }
 export type Plot = { _id: string; datas: Datas; config: Config }
 export type DeplotOptions = {
 	closeCallback: () => unknown
-	port: number
 }
 
 export type WebSocketMessage = {
@@ -29,32 +27,4 @@ export type WebSocketMessage = {
 		| { action: 'close' | 'screenshot' }
 		| { event: 'success' | 'error' }
 	result: string
-}
-
-export interface UIWorker extends Worker {
-	onmessage<T = unknown>(e: MessageEvent<WorkerThreadMessage<T>>): void
-	postMessage<T = unknown>(
-		message: MainThreadMessage<T>,
-		transferable: Transferable[],
-	): void
-	postMessage<T = unknown>(
-		message: MainThreadMessage<T>,
-		options?: StructuredSerializeOptions | undefined,
-	): void
-}
-
-type Actions = keyof Webview | '__constructor__'
-
-export type MainThreadMessage<T = unknown[]> = {
-	type: 'terminate'
-} | {
-	type: 'execute'
-	action: { name: Actions; args: T }
-}
-
-export type WorkerThreadMessage<T = unknown> = {
-	type: 'result'
-	return?: { name: Actions; value: T }
-} | {
-	type: 'terminate' | 'start'
 }
