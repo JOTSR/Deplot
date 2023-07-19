9375d36b89b2d66a6660635e9a1b97775571f0c2
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-07-19 02:52:06 +0200

    refactor: :recycle: move Deplot to WebUI for UI rendering
    Closes #1

--- src/deplot.ts
+++ src/deplot.ts
@@ -1,252 +1,78 @@
-import {
-	ensureFile,
-	path,
-	serve,
-	SizeHint,
-	WebSocketClient,
-	WebSocketServer,
-	Webview,
-} from '../deps.ts'
-import {
-	Config,
-	Datas,
-	DeplotOptions,
-	Plot,
-	PlotEngine,
-	UIWorker,
-	WorkerThreadMessage,
-} from './types.ts'
-import { copyObj, parseMessage, stringifyMessage } from './helpers.ts'
+import { ensureFile, path, WebUI } from '../deps.ts'
+import { Config, Datas, DeplotOptions, PlotEngine } from './types.ts'
 
 import { bundle } from '../public/bundle.ts'
 import { plotly } from '../public/plotly.ts'
 
-function bundleUI(request: Request): Response {
-	const fileName = (new URL(request.url)).pathname
-	if (fileName.endsWith('.ico')) return new Response(null)
-	if (fileName.endsWith('plotly.js')) {
-		return new Response(plotly, {
-			headers: {
-				'content-type': 'text/javascript; charset=utf-8',
-			},
-		})
-	}
-	return new Response(bundle, {
-		headers: {
-			'content-type': 'text/html; charset=utf-8',
-		},
-	})
-}
-
-const workerOptions: WorkerOptions = {
-	type: 'module',
-	deno: {
-		permissions: {
-			net: [
-				'0.0.0.0',
-				'127.0.0.1',
-				'localhost',
-			],
-			read: true,
-			write: true,
-			env: [
-				'PLUGIN_URL',
-				'DENO_DIR',
-				'LOCALAPPDATA',
-			],
-			ffi: true,
-		},
-	},
+function fileHandler({ pathname }: URL) {
+	if (pathname === '/plotly.js') return plotly
+	throw new Error(`uknown path ${pathname}`)
 }
 
 export class Deplot {
 	#plotEngine: PlotEngine
 	#options: DeplotOptions
-	#windows: Set<string> = new Set()
-	#websockets: Map<string, WebSocketClient> = new Map()
-	#wsBuffer: Map<string, { datas: Datas; config: Config }> = new Map()
-	#workers: Map<string, UIWorker> = new Map()
+	#window: WebUI
+	#datas?: Datas
+	#config: Config
+	#html = bundle
 
 	constructor(
 		plotEngine: PlotEngine,
 		options: DeplotOptions = {
-			port: 3123,
 			closeCallback: () => undefined,
 		},
 	) {
 		this.#plotEngine = plotEngine
 		this.#options = { ...options }
+		this.#window = new WebUI()
+		this.#window.setFileHandler(fileHandler)
 
-		serve(bundleUI, { port: this.#options.port + 1 })
-
-		const wss = new WebSocketServer(options.port)
-
-		wss.on('connection', (ws: WebSocketClient) => {
-			ws.on('message', (message: string) => {
-				const { id, payload, result } = parseMessage(
-					message,
-				)
-				if (
-					'event' in payload &&
-					payload.event === 'success'
-				) {
-					this.#websockets.set(id, ws)
-					const payload = this.#wsBuffer.get(id)!
-					this.#wsBuffer.delete(id)
-					const message = stringifyMessage({
-						id,
-						payload,
-					})
-					ws.send(message)
-				}
-				if (
-					'event' in payload &&
-					payload.event === 'error'
-				) {
-					this.#wsBuffer.delete(id)
-					throw new Error(
-						`Error on connection from plot ${id}: ${result}`,
-					)
-				}
-			})
-		})
+		this.#config = { title: plotEngine, size: [500, 500] }
 	}
 
-	plot(datas: Datas, config: Config): Plot {
-		config = { title: config.title ?? 'deplot', size: config.size }
-
-		const windowId = crypto.randomUUID()
-		this.#windows.add(windowId)
-		const port = this.#options.port
-
-		const worker = new Worker(
-			import.meta.resolve('./worker.ts'),
-			workerOptions,
-		) as UIWorker
-		this.#workers.set(windowId, worker)
-
-		worker.addEventListener(
-			'message',
-			({ data }: MessageEvent<WorkerThreadMessage>) => {
-				switch (data.type) {
-					case 'start':
-						initWorker(worker, {
-							config,
-							windowId,
-							plotEngine: this.#plotEngine,
-							port,
-						})
-						break
-					case 'terminate':
-						this.#workers.delete(windowId)
-						this.#wsBuffer.delete(windowId)
-						this.#windows.delete(windowId)
-						this.#options.closeCallback()
-						break
-				}
-			},
-		)
-
-		this.#wsBuffer.set(windowId, {
-			datas: copyObj(datas),
-			config: copyObj(config),
-		})
-
-		return {
-			_id: windowId,
-			datas: copyObj(datas),
-			config: copyObj(config),
-		}
+	static wait() {
+		return WebUI.wait()
 	}
 
-	update({ _id, datas, config }: Plot): Plot {
-		const message = stringifyMessage({
-			id: _id,
-			payload: { datas, config },
-		})
-		this.#websockets.get(_id)?.send(message)
-		return { _id, datas, config }
+	plot(datas: Datas, config: Config): Deplot {
+		this.#datas = datas
+		this.#config = { ...this.#config, ...config }
+
+		this.#window.show(this.#html)
+		const it = setInterval(() => {
+			this.#window.run(`DeplotClient.engine = "${this.#plotEngine}"`)
+			this.#window.run(
+				`DeplotClient.plot(${JSON.stringify(datas)}, ${
+					JSON.stringify(config)
+				})`,
+			)
+			if (this.#window.isShown) clearInterval(it)
+		}, 500)
+		return this
 	}
 
-	close({ _id }: Pick<Plot, '_id'>) {
-		this.#workers.get(_id)?.terminate()
+	update(datas: Datas, config: Config): Deplot {
+		return this.plot({ ...this.#datas, ...datas }, config)
 	}
 
-	screenShot(
-		{ _id }: Pick<Plot, '_id'>,
-		fileName: string,
-		callback: (path: string) => unknown,
-	) {
-		const message = stringifyMessage({
-			id: _id,
-			payload: { action: 'screenshot' },
-		})
-		this.#websockets.get(_id)?.send(message)
-		this.#websockets.get(_id)?.on('message', (message: string) => {
-			const { payload, result } = parseMessage(message)
-			if (result === 'error') {
-				throw new Error(
-					`Unable to take screenShot: ${payload}`,
-				)
-			}
+	close(): Deplot {
+		this.#window.close()
+		return this
+	}
+
+	async screenShot(fileName: string, callback: (path: string) => unknown) {
+		try {
+			const image = await this.#window.script('takeScreenshot()')
 			const filePath = path.join(Deno.cwd(), fileName)
 			ensureFile(filePath)
-			//write file
+			await Deno.writeFile(filePath, new TextEncoder().encode(image))
 			callback(filePath)
-			throw new Error('Not implemented')
-		})
+		} catch (cause) {
+			throw new Error(
+				`Unable to take the screenShot`,
+				{ cause },
+			)
+		}
 	}
-}
-
-function initWorker(
-	worker: UIWorker,
-	{ config, windowId, plotEngine, port }: {
-		config: Config
-		windowId: string
-		plotEngine: PlotEngine
-		port: number
-	},
-) {
-	worker.postMessage<ConstructorParameters<typeof Webview>>({
-		type: 'execute',
-		action: {
-			name: '__constructor__',
-			args: [
-				false,
-				{
-					width: config.size[0],
-					height: config.size[1],
-					hint: SizeHint.MIN,
-				},
-			],
-		},
-	})
-
-	worker.postMessage<Parameters<Webview['navigate']>>({
-		type: 'execute',
-		action: {
-			name: 'navigate',
-			args: [
-				`http://localhost:${
-					port + 1
-				}/index.html?id=${windowId}&engine=${plotEngine}&port=${port}`,
-			],
-		},
-	})
-
-	worker.postMessage<[Webview['title']]>({
-		type: 'execute',
-		action: {
-			name: 'title',
-			args: [config.title ?? `Deplot - ${windowId}`],
-		},
-	})
-
-	worker.postMessage<Parameters<Webview['run']>>({
-		type: 'execute',
-		action: {
-			name: 'run',
-			args: [],
-		},
-	})
 }
