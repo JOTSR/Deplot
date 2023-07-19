85ecad9b7af1813eaa8c440aa60530a94a71136e
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-07-19 02:51:07 +0200

    refactor: :recycle: rewrite the build system and deprecate deno bundle

--- build.ts
+++ build.ts
@@ -1,33 +1,40 @@
-import { esbuild } from './deps.ts'
-
-await Deno.spawn(Deno.execPath(), {
-	args: ['bundle', '--no-check', './src/client.ts', './public/client.js'],
-})
+import { esbuild, httpFetch } from './deps.ts'
 
 const html = await Deno.readTextFile('./public/index.html')
 
+await esbuild.build({
+	bundle: true,
+	target: ['chrome90', 'firefox90', 'safari15'],
+	treeShaking: false,
+	format: 'esm',
+	entryPoints: ['./src/client.ts'],
+	outfile: './public/client.js',
+	minify: true,
+	plugins: [httpFetch],
+})
 const js = await Deno.readTextFile('./public/client.js')
-const mjs = (await esbuild.transform(js, { minify: true })).code
-const plotly = await Deno.readTextFile('./vendor/Plotly/plotly-2_11_1.min.js')
+// const plotly = await Deno.readTextFile('./vendor/Plotly/plotly-2_11_1.min.js')
 
-const bundle = html
-	.replace('<!-- client -->', `<script defer>${mjs}</script>`)
+const bundle = html.replace(
+	'<!-- client -->',
+	`<script type="module">${js}</script>`,
+)
+await Deno.writeTextFile('./public/bundle.html', bundle)
 
 const denoComments = `
   // deno-fmt-ignore-file
   // deno-lint-ignore-file`
 
 const json = JSON.stringify({ bundle })
+await Deno.remove('./public/bundle.html')
 await Deno.remove('./public/client.js')
 await Deno.writeTextFile(
 	'./public/bundle.ts',
 	`${denoComments} \n export const { bundle } = ${json}`,
 )
-await Deno.writeTextFile(
-	'./public/plotly.ts',
-	`${denoComments} \n export const { plotly } = ${
-		JSON.stringify({ plotly })
-	}`,
-)
+// await Deno.writeTextFile(
+// 	'./public/plotly.ts',
+// 	`${denoComments} \n export const { plotly } = ${JSON.stringify({ plotly })}`
+// )
 
 Deno.exit()
