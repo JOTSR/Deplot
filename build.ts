510775e4a3a5d33e0bc73fe372326aa9015342f1
Julien Oculi <xdrz.jot.sr@gmail.com>
2023-07-20 15:06:24 +0200

    build: :construction_worker: rewrite build system and embed favicon to bundle

--- build.ts
+++ build.ts
@@ -1,7 +1,13 @@
-import { esbuild, httpFetch } from './deps.ts'
+import { Base64, esbuild, httpFetch } from './deps.ts'
 
+// Loading assets
+console.log('Loading assets')
 const html = await Deno.readTextFile('./public/index.html')
+const icon = await Deno.readFile('./public/logo-min.png')
+const plotly = await Deno.readTextFile('./vendor/Plotly/plotly-2_11_1.min.js')
 
+// Building client.ts
+console.log('Building client.ts')
 await esbuild.build({
 	bundle: true,
 	target: ['chrome90', 'firefox90', 'safari15'],
@@ -13,28 +19,46 @@ await esbuild.build({
 	plugins: [httpFetch],
 })
 const js = await Deno.readTextFile('./public/client.js')
-// const plotly = await Deno.readTextFile('./vendor/Plotly/plotly-2_11_1.min.js')
 
-const bundle = html.replace(
-	'<!-- client -->',
-	`<script type="module">${js}</script>`,
-)
+// Bundling index.html
+console.log('Bundling index.html')
+const bundle = html
+	.replace(
+		'<!-- client -->',
+		`<script type="module">${js}</script>`,
+	)
+	.replace(
+		'__ICON__',
+		`data:image/png;base64,${Base64.encode(icon)}`,
+	)
 await Deno.writeTextFile('./public/bundle.html', bundle)
 
 const denoComments = `
   // deno-fmt-ignore-file
   // deno-lint-ignore-file`
 
-const json = JSON.stringify({ bundle })
+// Cleaning temp files
+console.log('Cleaning temp files')
 await Deno.remove('./public/bundle.html')
 await Deno.remove('./public/client.js')
+
+// Writing bundle
+console.log('Writing bundle')
 await Deno.writeTextFile(
 	'./public/bundle.ts',
-	`${denoComments} \n export const { bundle } = ${json}`,
+	`${denoComments} \n export const { bundle } = ${
+		JSON.stringify({ bundle })
+	}`,
 )
-// await Deno.writeTextFile(
-// 	'./public/plotly.ts',
-// 	`${denoComments} \n export const { plotly } = ${JSON.stringify({ plotly })}`
-// )
 
+// Writing Plotly bundle
+console.log('Writing Plotly bundle')
+await Deno.writeTextFile(
+	'./public/plotly.ts',
+	`${denoComments} \n export const { plotly } = ${
+		JSON.stringify({ plotly })
+	}`,
+)
+
+// Force exit to stop esbuild promises
 Deno.exit()
